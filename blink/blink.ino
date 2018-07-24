f6ef62db67b7df7f2be93534a1d5866302a8b6f4
Rory Hayes <roryohayes@gmail.com>
2018-07-23 21:40:13 -0700

    init tests

---
+++ blink/blink.ino
@@ -0,0 +1,15 @@
+#define PIN 16
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
+}
